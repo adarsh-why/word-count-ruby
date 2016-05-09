31df145a84ebecf224f1efa1c058a1b30fe791fc
Adarsh <adarsh.chndrn@gmail.com>
2016-05-09 11:05:12 +0530

    word list working

---
+++ README.md
@@ -0,0 +1,16 @@
+create a hash word_hash with key as word and value as count
+Keep a sample text file
+Read from text file line by line
+take our each words and check if that word is there in the hash we create(word_hash)
+if that word is there in hash, increment its count
+if that word is not in hash, then create the word with count as 1
+
+case 1: 
+consider that there is only one line in text file
+consider all the words are downcased
+
+case 2:
+avoid considerations in case 1
+consider there are no commas or other punctuations
+consider all words are in each space seperated 
+
---
+++ words.rb
@@ -0,0 +1,44 @@
+@word_hash = Hash.new
+
+def read_each_line_from_file
+	File.open('wordfile.txt', 'r') do |file|  
+  		while line = file.gets  
+    		take_words_from line  
+  		end  
+	end	
+end
+
+def take_words_from line
+	words = line.gsub(/[^[:word:]\s]/, '').split(" ")
+	words.map!{|word| word.downcase}
+	update_hash_with words
+end
+
+def update_hash_with words
+	words.each do |word|
+		if @word_hash.has_key? word
+			@word_hash[word] += 1
+		else 
+			@word_hash[word] = 1
+		end
+	end
+end
+
+def remove_common_words
+	common_words = ''
+	File.open('common-words.txt', 'r') do |file|  
+  		while line = file.gets  
+    		common_words = line.split(' ')  
+  		end  
+	end
+
+	common_words.each do |word|
+		@word_hash.reject! {|key| key == word}
+	end
+end
+
+read_each_line_from_file
+remove_common_words
+File.open('output.txt', 'w') do |file|   
+    file.puts @word_hash.sort_by {|word, count| count}.reverse
+end	
